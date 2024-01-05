c15502ce3c2e62660235f1dc98f64c0870078c4a
İmranov <semtexpert@gmail.com>
2024-01-05 19:39:45 +0300

    version 2.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
+    static boolean isPalindrom(String str) {
+        int i = 0;
+        int j = (str.length()) - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+
+        }
+        return true;
+
+    }
+
+    static boolean isPalindrom2(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse +=str.charAt(i);
+
+        }
+        if (str.equals(reverse))
+            return true;
+        else
+            return false;
+
+
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPalindrom2("asa"));
+        System.out.println("Girdiğiniz Kelime Palindromiktir.");
+
     }
 }
